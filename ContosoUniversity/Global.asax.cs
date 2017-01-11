f561248990244ccf42a069d538ba986da0467884
Richard Iyama <richardiyama@yahoo.com>
2017-01-11 20:16:02 +0100

    commit

---
+++ ContosoUniversity/Global.asax.cs
@@ -0,0 +1,27 @@
+﻿using ContosoUniversity.DAL;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Data.Entity.Infrastructure.Interception;
+
+namespace ContosoUniversity
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Database.SetInitializer(new SchoolInitializer());
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            DbInterception.Add(new SchoolInterceptorTransientErrors());
+            DbInterception.Add(new SchoolInterceptorLogging());
+        }
+    }
+}
---
+++ Global.asax.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using ContosoUniversity.DAL;
+using System.Data.Entity.Infrastructure.Interception;
+using System.Data.Entity;
+
+namespace ContosoUniversity
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+
+            Database.SetInitializer(new SchoolInitializer());
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            DbInterception.Add(new SchoolInterceptorTransientErrors());
+            DbInterception.Add(new SchoolInterceptorLogging());
+        }
+    }
+}
