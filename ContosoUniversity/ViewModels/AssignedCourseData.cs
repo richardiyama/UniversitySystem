ab64dc235038ca49ae4d30c317eb0789345edc75
Richard Iyama Oluwaseun <richardiyama@yahoo.com>
2017-07-29 06:06:38 +0100

    Update AssignedCourseData.cs

--- ContosoUniversity/ViewModels/AssignedCourseData.cs
+++ ContosoUniversity/ViewModels/AssignedCourseData.cs
@@ -7,6 +7,7 @@ namespace ContosoUniversity.ViewModels
 {
     public class AssignedCourseData
     {
+       
         public int CourseID { get; set;}
         public string Title { get; set; }
         public bool Assigned { get; set; }
